42304e57732c7deeed7d41911d340287ada29f88
ssfdust <ssfdust@gmail.com>
2019-11-11 18:02:44 +0800

    WIP

---
+++ Chapter_06/9.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate scan_fmt;
+extern crate promptly;
+
+use promptly::prompt;
+
+pub fn main() {
+    let mut inputs: String = prompt("Input two numbers");
+    loop {
+        let (n, m): (f64, f64) = match scan_fmt!(&inputs, "{} {}", f64, f64){
+            Ok((n, m)) => (n, m),
+            Err(_) => break
+        };
+        println!("{:.2}", calculate(n, m));
+        inputs = prompt("Input your next pair of numbers");
+    }
+    println!("Bye!")
+}
+
+fn calculate(n: f64, m: f64) -> f64 {
+    (n - m) / (n * m)
+}
